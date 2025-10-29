181d7d087ff565ba42101e9974a3455861a757d7
Alex Indri <axindri@gmail.com>
2025-10-29 22:23:06 +0300

    Update BrowserRouter configuration to support dynamic basename for production

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/index.css'
 import App from './App.tsx'
 
+const basename = import.meta.env.PROD ? '/frg_website' : '';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
